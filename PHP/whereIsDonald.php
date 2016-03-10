d0a8e144fb803b4e9085cfee2928c10cbf1acc5e
Sloan <sloan.fardele@outlook.fr>
2016-03-10 23:26:31 +0100

    Ajout de where is donald et modification du header et ajout du hamburger menu

---
+++ PHP/whereIsDonald.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sloan
+ * Date: 10/03/2016
+ * Time: 22:56
+ */
+
+include("header.html");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Where is Donald ?</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/styleWhereIs.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+</head>
+
+<body>
+<div>
+    <img src=../images/DonaldTrump.png id='donny' alt="" onclick="found()" >
+</div>
+</body>
+
+<script type="text/javascript" src="../JS/WhereIsDonald.js"></script>
+</html>
