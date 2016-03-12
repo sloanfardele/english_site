baba54d940314f3399f6a390a39e88924037b422
sloanfardele <sloan.fardele@outlook.fr>
2016-03-12 12:15:20 +0100

    Ajout d'une alerte si donnie se met a gauchev2

--- JS/WhereIsDonald.js
+++ JS/WhereIsDonald.js
@@ -18,20 +18,12 @@ function posAleat() {
     posx = posx % image.offsetWidth;
     posy = posy % image.offsetHeight;
 
-    console.log("posx");
-    console.log(posx);
-    console.log("tete");
-    console.log(tete.offsetWidth);
-
     posx = posx - tete.offsetWidth;
 
     if(posx<0)
     {
         alert("Erreur, rafraichir s'il vous plait(Ctrl+R ou F5");
     }
-
-
-
 
     document.getElementById("donny").style.marginLeft = posx + "px";
     document.getElementById("donny").style.marginTop = posy + "px";
