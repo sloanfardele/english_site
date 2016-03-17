715bc0fcf3703ae94e5ea2e8f1852b171fcd8486
sloanfardele <sloan.fardele@outlook.fr>
2016-03-17 10:20:02 +0100

    creation du donaldjump

---
+++ PHP/donaldjump.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: link0
+ * Date: 17/03/2016
+ * Time: 09:44
+ */
+
+include("header.html");
+
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../CSS/donaldjump.css">
+</head>
+<body>
+
+<script type="text/javascript" src="../JS/donaldjump.js"></script>
+</body>
+</html>
