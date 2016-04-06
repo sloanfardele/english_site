50f3e2166c60f58866841154553ba8a7d7e033af
ALLIAUME Hugo <kocal@live.fr>
2016-04-06 15:12:54 +0200

    Le compteur est un fichier .txt afin d'être interprété correctement par le browser

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <?php
-$counter = 'counter';
+$counter = 'counter.txt';
 
-if(!file_exists('counter')) {
+if(!file_exists($counter)) {
     file_put_contents($counter, 0);
 }
 
-file_put_contents($counter, 1 + file_get_contents('counter'));
+file_put_contents($counter, 1 + file_get_contents($counter));
 ?>
 <head>
     <link rel="stylesheet" type="text/css" href="/CSS/style.css"/>
